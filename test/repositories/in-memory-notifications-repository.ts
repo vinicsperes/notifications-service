b809fd0c509c7111764e9c99eae1cd85bdc86c69
ohperes <peresvini2@gmail.com>
2022-12-17 15:16:47 -0300

    feat: implement linsting by recipient

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -27,6 +27,13 @@ export class InMemoryNotificationsRepository
       (notification) => notification.recipientId === recipientId,
     ).length;
   }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
+
   async save(notification: Notification): Promise<void> {
     const notificationIndex = this.notifications.findIndex(
       (item) => item.id === notification.id,
